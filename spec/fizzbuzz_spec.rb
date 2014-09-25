ac0c7005f4acd28d59e63d8008ddd12ba16f99f7
RachelNolan <mail@rachelnolan.com>
2014-09-25 21:10:09 +0100

    Knows when number divisible by five

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,6 +10,11 @@ context "number should know when it is" do
 		expect(fizzbuzz.divisible_by_three?(3)).to be true
 	end
 
+	it "divisible_by_five" do 
+		expect(fizzbuzz.divisible_by_five?(5)).to be true
+	end
+
+
 end
 
 
