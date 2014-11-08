0999edf43edcc91941cec5229ff6c5aa3fadddf8
RachelNolan <mail@rachelnolan.com>
2014-11-08 22:46:18 +0000

    knows when a number is/is not divisible by five

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,10 @@
 class Fizzbuzz
 
-	def is_divisible_by_three?(number)  
+	def divisible_by_three?(number)  
 		number % 3 == 0 
-
 	end 
 
+	def divisible_by_five?(number)  
+		number % 5 == 0 
+	end 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,15 +7,23 @@ describe 'Fizzbuzz' do
 	context ' should know when a number is' do 
 
 		it 'divisible by three' do 
-			expect(fizzbuzz.is_divisible_by_three?(3)).to equal(true)
+			expect(fizzbuzz.divisible_by_three?(3)).to equal(true)
+		end 
+
+		it 'divisible by five' do 
+			expect(fizzbuzz.divisible_by_five?(5)).to equal(true)
 		end 
 	end 
-	
+
 	let(:fizzbuzz) {Fizzbuzz.new}
 
 	context 'should know when a number is NOT' do 
 		it 'divisible by three' do 
-			expect(fizzbuzz.is_divisible_by_three?(2)).to equal(false)
+			expect(fizzbuzz.divisible_by_three?(2)).to equal(false)
+		end 
+
+		it 'divisible by five' do 
+			expect(fizzbuzz.divisible_by_five?(1)).to equal(false)
 		end 
 	end 
 
