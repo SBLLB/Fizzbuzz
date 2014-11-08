7e3a621c32a3cb60a2f1b8c1e57329256758e9c5
RachelNolan <mail@rachelnolan.com>
2014-11-08 22:43:59 +0000

    Knows when a number is NOT divisible by three

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,11 +2,20 @@ require 'fizzbuzz'
 
 describe 'Fizzbuzz' do 
 
+	let(:fizzbuzz) {Fizzbuzz.new}
+
 	context ' should know when a number is' do 
 
 		it 'divisible by three' do 
-			fizzbuzz = Fizzbuzz.new
 			expect(fizzbuzz.is_divisible_by_three?(3)).to equal(true)
+		end 
+	end 
+	
+	let(:fizzbuzz) {Fizzbuzz.new}
+
+	context 'should know when a number is NOT' do 
+		it 'divisible by three' do 
+			expect(fizzbuzz.is_divisible_by_three?(2)).to equal(false)
 		end 
 	end 
 
