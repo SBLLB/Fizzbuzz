f43f22ebd9d0cd509abb41ba42a8b724e4576dbc
RachelNolan <mail@rachelnolan.com>
2015-02-23 18:31:25 +0100

    Knows when a number is divisible by three

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,14 @@
+require 'fizzbuzz'
+
+describe 'Fizzbuzz' do 
+
+	context 'should know when a number is' do
+
+		it 'divisible by three' do 
+			fizzbuzz = Fizzbuzz.new
+			expect(fizzbuzz.divisible_by_three?(3)).to equal(true)
+		end  
+
+	end
+
+end 
