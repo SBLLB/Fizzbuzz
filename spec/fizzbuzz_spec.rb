eb393e36af1a02d951ae046f634bfd2dff5577fe
RachelNolan <mail@rachelnolan.com>
2015-02-23 18:37:17 +0100

    Knows when a number is/is NOT divisible by fifteen

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,9 +13,14 @@ describe 'Fizzbuzz' do
 		it 'divisible by five' do 
 			expect(fizzbuzz.divisible_by_five?(5)).to equal(true)
 		end  
+
+		it 'divisible by fifteen' do 
+			expect(fizzbuzz.divisible_by_fifteen?(15)).to equal(true)
+		end  
 	end
 
 	context 'should know when a number is' do 
+
 		it 'NOT divisible by three' do 
 			expect(fizzbuzz.divisible_by_three?(1)).to equal(false)
 		end 
@@ -23,6 +28,11 @@ describe 'Fizzbuzz' do
 		it 'NOT divisible by five' do 
 			expect(fizzbuzz.divisible_by_five?(1)).to equal(false)
 		end 
+
+		it 'NOT divisible by fifteen' do 
+			expect(fizzbuzz.divisible_by_fifteen?(1)).to equal(false)
+		end 
+
 	end 
 
 end 
