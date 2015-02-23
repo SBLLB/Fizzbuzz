669da0fdc6b02ec2816375f1337dfe6665324867
RachelNolan <mail@rachelnolan.com>
2015-02-23 18:35:51 +0100

    Knows when a number is NOT divisible by 5 & refactoring test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,23 +2,26 @@ require 'fizzbuzz'
 
 describe 'Fizzbuzz' do 
 
+	let(:fizzbuzz) {Fizzbuzz.new}
+
 	context 'should know when a number is' do
 
 		it 'divisible by three' do 
-			fizzbuzz = Fizzbuzz.new
 			expect(fizzbuzz.divisible_by_three?(3)).to equal(true)
 		end  
 
 		it 'divisible by five' do 
-			fizzbuzz = Fizzbuzz.new
 			expect(fizzbuzz.divisible_by_five?(5)).to equal(true)
 		end  
 	end
 
 	context 'should know when a number is' do 
 		it 'NOT divisible by three' do 
-			fizzbuzz = Fizzbuzz.new
 			expect(fizzbuzz.divisible_by_three?(1)).to equal(false)
+		end 
+
+		it 'NOT divisible by five' do 
+			expect(fizzbuzz.divisible_by_five?(1)).to equal(false)
 		end 
 	end 
 
