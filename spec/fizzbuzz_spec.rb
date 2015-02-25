ce8f12476e2587d518058f431eef9750453878fc
RachelNolan <mail@rachelnolan.com>
2015-02-25 22:41:04 +0000

    Knows whe a number is NOT divisible by three

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,6 +10,14 @@ describe 'Fizzbizz' do
     end
 
   end
+  
+  context 'should know when a number is NOT' do
 
+    it 'divisible by three' do 
+      fizzbuzz = Fizzbuzz.new
+      expect(fizzbuzz.divisible_by_three?(1)).to equal(false)
+    end
+
+  end
 
 end 
